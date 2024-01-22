7902480de5fa76f47ff0ebc59dc97c66b8268519
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2024-01-22 13:54:18 +0100

    API for mqtt server

--- Server/main.py
+++ Server/main.py
@@ -5,6 +5,11 @@ import paho.mqtt.client as mqtt
 from fastapi import FastAPI
 from recognizer import Recognizer
 
+
+master_tags=["07081075dcdfa5b91b5c8aac1402f05d28b68563c026f0fe03412c087bf41c9f", "9bc6180d675480f93a205652c8477f350aeecd0c21618290c618ba0dfaec9ed5"]
+pin="b2cf84c62d338adb214f88c9e56a42a8e87c17ae5f7eb176bb248b3cb6baa4f7"
+isAuthorized=False
+
 host = "localhost"
 port = 1883
 keepalive = 30
@@ -23,8 +28,22 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, user_data, msg):
     if msg.topic == receive_topic:
         data = json.loads(msg.payload.decode())
-        print(data)
-
+        if data.get("action",None) == "pinUnlock":
+            pin_input=data.get("pin",None)
+            if (pin_input==pin):
+                isAuthorized=True
+            client.publish(send_topic,json.dumps({"authorized":isAuthorized}))
+        if data.get("action",None) == "tagUnlock":
+            tag_input=data.get("tag",None)
+            if (tag_input in master_tags):
+                isAuthorized=True
+            client.publish(send_topic,json.dumps({"authorized":isAuthorized}))
+        if data.get("action",None) == "lock":
+            isAuthorized=False
+            client.publish(send_topic,json.dumps({"authorized":isAuthorized}))
+        if data.get("action",None) == "pir":
+            state = data.get("state", None)
+            recognizer.recognition_active=state
 
 client.on_message = on_message
 client.on_connect = on_connect
