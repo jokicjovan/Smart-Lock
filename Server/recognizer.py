86d11d515b510d6be6d01fdd1f4d398551b56a3a
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2024-01-23 23:50:37 +0100

    Server adapted to work with model

--- Server/recognizer.py
+++ Server/recognizer.py
@@ -1,29 +1,29 @@
 import asyncio
-import os
 import cv2
+from model import VGGFaceModel
 
-from siamese_model import SiameseModel
+
+def get_face_classifier():
+    return cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+
+
+def get_ann_model():
+    return VGGFaceModel()
 
 
 class Recognizer:
     def __init__(self):
-        self.allowed_faces = []
-        allowed_faces_path = "data/allowed"
-        for filename in os.listdir(allowed_faces_path):
-            if filename.endswith(".jpg") or filename.endswith(".png"):
-                file_path = os.path.join(allowed_faces_path, filename)
-                self.allowed_faces.append(cv2.resize(cv2.cvtColor(cv2.imread(file_path), cv2.COLOR_BGR2GRAY),
-                                                     (64, 64), interpolation=cv2.INTER_NEAREST))
-
         self.recognition_active = False
         self.stream_url = "http://192.168.1.177:81/stream"
-        self.classifier = self.get_face_classifier()
-        self.ann = self.get_snn()
+        self.classifier = get_face_classifier()
+        self.ann = get_ann_model()
 
-    async def check_stream(self):
+    async def check_for_verified_person(self):
         cap = cv2.VideoCapture(self.stream_url)
         is_valid = False
-        while True:
+        while not is_valid:
             if not self.recognition_active:
                 break
 
@@ -39,9 +39,11 @@ class Recognizer:
 
             for (x, y, w, h) in face:
                 face_roi = frame[y:y + h, x:x + w]
-                face_roi = cv2.resize(face_roi, (64, 64), interpolation=cv2.INTER_NEAREST)
-                is_valid = self.check_face(face_roi)
-                if is_valid:
+                face_roi = cv2.resize(face_roi, (224, 224), interpolation=cv2.INTER_CUBIC)
+                face = self.ann.check_face(face_roi)
+                print(face)
+                if face is not None:
+                    is_valid = True
                     break
 
             # for (x, y, w, h) in face:
@@ -55,17 +57,33 @@ class Recognizer:
         cv2.destroyAllWindows()
         return is_valid
 
-    def check_face(self, face_roi):
-        for face in self.allowed_faces:
-            similarity = self.ann.calculate_similarity(face, face_roi)
-            if similarity > 0.7:
-                return True
+    async def add_verified_person(self):
+        cap = cv2.VideoCapture(self.stream_url)
+        new_person_faces = []
+        while len(new_person_faces) < 10:
+            ret, frame = cap.read()
+            if not ret:
+                print("Failed to receive frame. Exiting...")
+                break
+
+            gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            face = self.classifier.detectMultiScale(
+                gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+            )
+
+            for (x, y, w, h) in face:
+                face_roi = frame[y:y + h, x:x + w]
+                face_roi = cv2.resize(face_roi, (224, 224), interpolation=cv2.INTER_CUBIC)
+                face = self.ann.check_face(face_roi)
+                if face is None:
+                    new_person_faces.append(face_roi)
+
+            await asyncio.sleep(0.00001)
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+        if len(new_person_faces) >= 10:
+            self.ann.add_face(new_person_faces)
+            return True
         return False
-
-    def get_face_classifier(self):
-        return cv2.CascadeClassifier(
-            cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-        )
-
-    def get_snn(self):
-        return SiameseModel()
