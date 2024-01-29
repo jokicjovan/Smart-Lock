ac8893ecfb679d43645dfdf0ac4ba894bfa6e462
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2024-01-29 19:52:57 +0100

    Tests added

--- Server/main.py
+++ Server/main.py
@@ -2,7 +2,6 @@ import asyncio
 import json
 import paho.mqtt.client as mqtt
 from fastapi import FastAPI
-from hashlib import sha256
 from recognizer import Recognizer
 import logging
 
@@ -15,11 +14,12 @@ receive_topic = f"FromLock"
 app = FastAPI()
 app.isAuthorized = True
 app.recognizer = Recognizer()
-app.incorrect_tries=0
-master_tags = ["781075dcdfa5b91b5c8aac142f05d28b68563c026f0fe3412c87bf41c9f", "9bc618d675480f93a205652c8477f35aeecdc21618290c618badfaec9ed5"]
+app.incorrect_tries = 0
+master_tags = ["781075dcdfa5b91b5c8aac142f05d28b68563c026f0fe3412c87bf41c9f",
+               "9bc618d675480f93a205652c8477f35aeecdc21618290c618badfaec9ed5"]
 app.pin = "b2cf84c62d338adb214f88c9e56a42a8e87c17ae5f7eb176bb248b3cb6baa4f7"
 client = mqtt.Client(client_id="11111111", clean_session=True)
-loop=asyncio.get_event_loop()
+loop = asyncio.get_event_loop()
 logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.getLogger("requests").setLevel(logging.WARNING)
 
@@ -35,32 +35,32 @@ def on_message(client, user_data, msg):
             pin_input = data.get("pin", None)
             if pin_input == app.pin:
                 app.isAuthorized = True
-                app.recognizer.recognition_active=False
-                app.incorrect_tries=0
+                app.recognizer.recognition_active = False
+                app.incorrect_tries = 0
                 logging.info(f"Successful unlock by pin: {pin_input}")
             else:
-                app.incorrect_tries+=1
+                app.incorrect_tries += 1
                 logging.warning(f"Unsuccessful unlock by pin: {pin_input}")
-            if app.incorrect_tries>=3:
+            if app.incorrect_tries >= 3:
                 loop.create_task(app.recognizer.capture())
                 logging.warning(f"Alarm is on!")
             client.publish(send_topic, json.dumps({"authorized": app.isAuthorized}))
         if data.get("action", None) == "newPin":
             pin_input = data.get("pin", None)
-            old_pin=app.pin
-            app.pin=pin_input
+            old_pin = app.pin
+            app.pin = pin_input
             logging.info(f"Pin changed from {old_pin} to {pin_input}")
         if data.get("action", None) == "tagUnlock":
             tag_input = data.get("tag", None)
             if tag_input in master_tags:
                 app.isAuthorized = True
-                app.recognizer.recognition_active=False
+                app.recognizer.recognition_active = False
                 app.incorrect_tries = 0
                 logging.info(f"Successful unlock by tag: {tag_input}")
             else:
                 app.incorrect_tries += 1
                 logging.warning(f"Unsuccessful unlock by tag: {tag_input}")
-            if app.incorrect_tries>=3:
+            if app.incorrect_tries >= 3:
                 loop.create_task(app.recognizer.capture())
                 logging.warning(f"Alarm is on!")
 
@@ -71,7 +71,7 @@ def on_message(client, user_data, msg):
             logging.info(f"Locked")
         if data.get("action", None) == "pir":
             state = data.get("state", None)
-            app.recognizer.recognition_active=state
+            app.recognizer.recognition_active = state
             if state:
                 loop.create_task(check_stream())
                 logging.info(f"Face recognition active")
@@ -98,7 +98,6 @@ async def check_stream():
                        retain=False)
 
 
-
 async def add_person_from_stream():
     success = await app.recognizer.add_verified_person()
     if success:
@@ -112,8 +111,6 @@ async def add_person_from_stream():
         logging.info(f"Failed to add new face")
 
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
---
+++ Server/test.py
@@ -0,0 +1,111 @@
+import asyncio
+import os
+import cv2
+import httpx
+import sklearn.datasets
+from PIL import Image
+from datetime import datetime
+from io import BytesIO
+
+from resnet_model import ResNet50
+
+
+async def capture(url):
+    for i in range(0, 300):
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url + "/capture")
+            img_stream = BytesIO(response.content)
+            img = Image.open(img_stream)
+            img.save(os.path.join("test", "capture", datetime.now().strftime("%Y%m%d%H%M%S") + ".png"))
+            await asyncio.sleep(1)
+
+
+async def load_images_from_path(imgs_path):
+    images = []
+    for filename in os.listdir(imgs_path):
+        if filename.endswith("png") or filename.endswith("jpg") or filename.endswith("jpeg"):
+            path = os.path.join(imgs_path, filename)
+            img = cv2.imread(path)
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            classifier = cv2.CascadeClassifier(
+                cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+            )
+            face = classifier.detectMultiScale(
+                img_gray, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+            )
+
+            for (x, y, w, h) in face:
+                face_roi = img[y:y + h, x:x + w]
+                images.append(face_roi)
+    return images
+
+
+async def get_true_pos_false_neg(images_dataset, model):
+    false_neg = 0
+    for image in images_dataset:
+        person = model.check_authorization(image)
+        if person is None:
+            false_neg += 1
+    return len(images_dataset) - false_neg, false_neg
+
+
+async def get_false_pos(images_dataset, model):
+    false_pos = 0
+    num_of_imgs = 0
+    for image in images_dataset:
+        person = model.check_authorization(image)
+        if person is not None:
+            false_pos += 1
+        num_of_imgs += 1
+    return false_pos
+
+
+async def calculate_false_rejection_rate(false_negatives, num_of_imgs):
+    return round(false_negatives * 100 / num_of_imgs, 2)
+
+
+async def calculate_false_acceptance_rate(false_neg, num_of_imgs):
+    return round(false_neg * 100 / num_of_imgs, 2)
+
+
+async def calculate_precision(true_positives, false_positives):
+    return round(true_positives / (true_positives + false_positives), 2)
+
+
+async def calculate_recall(true_positives, false_negatives):
+    return round(true_positives / (true_positives + false_negatives), 2)
+
+
+async def calculate_f1(precision, recall):
+    return round(2 * precision * recall / (precision + recall), 2)
+
+
+async def main():
+    # await capture("http://192.168.1.177")
+
+    model = ResNet50()
+
+    images_dataset1 = await load_images_from_path("test/authorized")
+    true_positives, false_negatives = await get_true_pos_false_neg(images_dataset1, model)
+
+    data = sklearn.datasets.fetch_lfw_people(color=True)
+    images_dataset2 = data["images"][:300]
+    # images_dataset2 = await load_images_from_path("test/unauthorized")
+    false_positives = await get_false_pos(images_dataset2, model)
+
+    ffr = await calculate_false_rejection_rate(false_negatives, len(images_dataset1))
+    far = await calculate_false_acceptance_rate(false_positives, len(images_dataset2))
+    precision = await calculate_precision(true_positives, false_positives)
+    recall = await calculate_recall(true_positives, false_negatives)
+    f1 = await calculate_f1(precision, recall)
+
+    print(f"tested on {len(images_dataset1)} images of authorized person")
+    print(f"tested on {len(images_dataset2)} images of unauthorized person")
+    print(f"FRR: {ffr} %")
+    print(f"FAR: {far} %")
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
+    print(f"F1: {f1}")
+
+
+asyncio.run(main())
